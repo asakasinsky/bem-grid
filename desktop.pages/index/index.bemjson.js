b4927e8c23bc9927c23fa5540e5dd2ba382e182d
Anton Winogradov <winogradovaa@gmail.com>
2014-09-14 22:09:27 +0400

    fix paths

--- desktop.pages/index/index.bemjson.js
+++ desktop.pages/index/index.bemjson.js
@@ -6,7 +6,7 @@
         { elem : 'meta', attrs : { name: 'viewport', content: 'width=device-width, initial-scale=1' } }
     ],
     styles : [
-        { elem : 'css', url : '_index.css' }
+        { elem : 'css', url : 'https://raw.githubusercontent.com/verybigman/bem-grid/gh-pages/desktop.pages/index/_index.css' }
     ],
     content : [
         {
@@ -593,5 +593,5 @@
             ]
         }
     ],
-    scripts : [{ elem : 'js', url : '_index.js' }]
+    scripts : [{ elem : 'js', url : 'https://raw.githubusercontent.com/verybigman/bem-grid/gh-pages/desktop.pages/index/_index.css' }]
 })
